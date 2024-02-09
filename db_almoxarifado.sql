064f56d9ba49e8f0048957208e6c132672c8b628
DanielJairton <daniel.jairton679@gmail.com>
2024-02-09 10:22:52 -0300

    Update

--- db_almoxarifado.sql
+++ db_almoxarifado.sql
@@ -7,6 +7,7 @@ USE db_almoxarifado
 
 --Criando Tabela Fornecedores
 --Dados dos fornecedores dos itens
+--Incompleto falta decidir se endereço é composto ou simples
 CREATE TABLE Fornecedores(
 id_fornecedor INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(90) NOT NULL,
@@ -22,22 +23,35 @@ email VARCHAR(70) NOT NULL
 );
 
 --Criando Tabela Itens
+--Completa
 CREATE TABLE Itens(
 id_item INT PRIMARY KEY IDENTITY(1,1),
 nome VARCHAR(90) NOT NULL,
-descricao VARCHAR(300)
+descricao VARCHAR(500)
+);
+
+--Criando Tabela Funcionarios
+--Completo
+CREATE TABLE Funcionarios (
+id_funcionario INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(100) NOT NULL,
+cargo VARCHAR(50) NOT NULL,
+telefone VARCHAR(20) NOT NULL,
+email VARCHAR(10) NOT NULL
 );
 
 --Criando Tabela Lotes
 --Dados dos lotes que formam o estoque
+--Completa
 CREATE TABLE Lotes(
 id_lote INT PRIMARY KEY IDENTITY(1,1),
 preco_unitario DECIMAL(10,2) NOT NULL,
-quantidade INT NOT NULL,
-localizacao VARCHAR(90) NOT NULL,
-data_recebimento DATE NOT NULL,
+quantidade_recebimento INT NOT NULL,
+quantidade_atual INT NOT NULL,
+condicao_recebimento VARCHAR(100) NOT NULL,
+data_recebimento DATE,
 data_validade DATE,
-condicao_entrega VARCHAR(100) NOT NULL,
+localizacao VARCHAR(90) NOT NULL,
 id_item INT NOT NULL,
 id_fornecedor INT NOT NULL,
 FOREIGN KEY(id_item) REFERENCES Itens(id_item),
@@ -45,6 +59,7 @@ FOREIGN KEY(id_fornecedor) REFERENCES Fornecedores(id_fornecedor),
 );
 
 --Criando Tabela Entradas
+--Incompleta
 CREATE TABLE Entradas(
 id_entrada INT PRIMARY KEY IDENTITY(1,1),
 
